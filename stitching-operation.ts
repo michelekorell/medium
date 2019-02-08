6d2e2b8ced07abcc49a3d51109cadc71c81cc7ae
Michel Korell <michele.korell@hotmail.ch>
2019-02-08 02:24:23 +0100

    Added comments

--- stitching-operation.ts
+++ stitching-operation.ts
@@ -2,11 +2,14 @@ import { graphql, GraphQLSchema, GraphQLResolveInfo } from "graphql";
 import { delegateToSchema, Transform } from "graphql-tools";
 
 export class Connector {
+    //Constructor get remote schema
     constructor(private schema: GraphQLSchema) {
     }
 
+    //Resource query is a remote endpoint operation
     ResourceCreate(args: any, context: any, info: GraphQLResolveInfo): Promise<any> {
 
+        //use of delegation on remote
         let fieldName = "ResourceCreate";
         return delegateToSchema({
             schema: this.schema,
@@ -19,15 +22,16 @@ export class Connector {
 
     }
 
+    //Local query
     addResources(args: any, context: any, info: GraphQLResolveInfo, transforms?: Array<Transform>): Promise<any> {
-        //Rewrite of different query adapted to next microservice
+        //That need to rewruite a different query adapted to next microservice
         const mutation = `mutation ResourceCreate($proto: Resource) {
             ResourceCreate(resource: $proto) {
               id
             }
           }`;
 
-        //Promise for resource create
+        //Promise for resources resolution
         return new Promise((resolve) => {
             graphql(this.schema, mutation, null, context, args).then((resource: any) => {
                 resolve(resource.data.ResourceCreate);
